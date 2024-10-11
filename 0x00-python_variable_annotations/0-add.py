131b7a24fae4a393b7d6805f2ae43881e59b3f3b
LibrazionPc <znwajei@gmail.com>
2024-10-11 13:15:04 +0100

    Git Issues

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""Basic annotations for variables Annotations."""
+
+
+def add(a: float, b: float) -> float:
+    '''
+        Returns sum of two floats.
+    '''
+    return a + b
